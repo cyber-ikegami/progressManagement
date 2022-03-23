023ccf58ac8430ce65af477261d08e57affa4cfd
cyber-ikegami <ikegami@cyberbeing.tech>
2022-03-23 13:35:24 +0900

    0323進捗状況01

--- src/system/utils/systemUtil.tsx
+++ src/system/utils/systemUtil.tsx
@@ -3,9 +3,14 @@ namespace SystemUtil {
     export const TAB_FONT_SIZE = 15;
     export const TAB_WEDTH = 100;
     export const TAB_HEIGTH = 20;
+
     export const HEADER_HEIGTH = 40;
+
     export const DAIGAKU_LABEL_HEIGTH = 30;
-    export const DAIGAKU_CHAR_SIZE = 20;
+
+    export const ANKEN_LABEL_HEIGTH = 60;
+
+    export const CONTENTS_CHAR_SIZE = 20;
 }
 
 export default SystemUtil;
