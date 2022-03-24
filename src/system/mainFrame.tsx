3fc2d3279cb2409d576e4959e17924a1e52fa714
cyber-ikegami <ikegami@cyberbeing.tech>
2022-03-24 11:05:39 +0900

    0324進捗状況01

--- src/system/mainFrame.tsx
+++ src/system/mainFrame.tsx
@@ -5,37 +5,37 @@ import DaigakuTab from './daigakuTab';
 import AnkenTab from './ankenTab';
 
 const MainFrame = () => {
-  // 画面遷移の管理(大学、案件)
-  type Mode = 'daigaku' | 'anken';
-  // 画面遷移の管理
-  const [mode, setMode] = useState<Mode>('daigaku');
+    // 画面遷移の管理(大学、案件)
+    type Mode = 'daigaku' | 'anken';
+    // 画面遷移の管理
+    const [mode, setMode] = useState<Mode>('daigaku');
 
-  // 画面の状態を管理する
-  let contentsJsx = <></>;
+    // 画面の状態を管理する
+    let contentsJsx = <></>;
 
-  // 画面切り替え
-  switch (mode) {
-    case 'daigaku':
-      contentsJsx = <DaigakuTab></DaigakuTab>;
-    break;
-    case 'anken':
-      contentsJsx = <AnkenTab></AnkenTab>;
-    break;
-  }
+    // 画面切り替え
+    switch (mode) {
+        case 'daigaku':
+            contentsJsx = <DaigakuTab></DaigakuTab>;
+            break;
+        case 'anken':
+            contentsJsx = <AnkenTab></AnkenTab>;
+            break;
+    }
 
-  return (
-    <>
-      <_Tab>
-        <_ModeItem isActive={mode === 'daigaku'} onClick={() => {
-          setMode('daigaku');
-        }} >大学</_ModeItem>
-        <_ModeItem isActive={mode === 'anken'} onClick={() => {
-          setMode('anken');
-        }} >案件</_ModeItem>
-      </_Tab>
-      <_Contents>{contentsJsx}</_Contents>
-    </>
-  );
+    return (
+        <>
+            <_Tab>
+                <_ModeItem isActive={mode === 'daigaku'} onClick={() => {
+                    setMode('daigaku');
+                }} >大学</_ModeItem>
+                <_ModeItem isActive={mode === 'anken'} onClick={() => {
+                    setMode('anken');
+                }} >案件</_ModeItem>
+            </_Tab>
+            <_Contents>{contentsJsx}</_Contents>
+        </>
+    );
 }
 
 export default MainFrame;
@@ -49,7 +49,7 @@ const _Tab = styled.div`
 
 // 状態を示すラベル
 const _ModeItem = styled.div<{
-  isActive: boolean;
+    isActive: boolean;
 }>`
   cursor: pointer;
   background-color: ${props => props.isActive ? '#58e85c' : '#9fe6a1'};
